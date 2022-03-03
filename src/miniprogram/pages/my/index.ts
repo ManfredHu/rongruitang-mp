f265795f095b02c728c20164e5ff447c915cb0b3
pphu <pphu@tencent.com>
2022-03-03 22:34:53 +0800

    feat: my page

--- src/miniprogram/pages/my/index.ts
+++ src/miniprogram/pages/my/index.ts
@@ -33,15 +33,48 @@ import packageJson from "../../../../package.json";
 export default class MyPage extends BasePage {
   readonly name = "page/my/index";
 
-  data = { kfQrCode, myName: "游客", version: packageJson.version }
+  data = {
+    kfQrCode,
+    version: packageJson.version,
+    userInfo: {
+      nickName: "游客",
+      gender: 0
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+  };
 
   onLoad() {
-    // if (wx.getUserProfile) {
-    //   this.setData({
-    //     canIUseGetUserProfile: true,
-    //   });
-    // }
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true,
+      });
+    }
   }
+
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: "用于展示头像昵称信息", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res: WechatMiniprogram.GetUserProfileSuccessCallbackResult) => {
+        console.log(`wx.getUserProfile res`, res);
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        });
+      },
+    });
+  }
+
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+    });
+  }
+
   onShow() {
     this.setTabBar({
       selected: 2,
